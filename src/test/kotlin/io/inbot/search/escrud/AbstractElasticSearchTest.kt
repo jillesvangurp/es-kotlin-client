ad62f39679dc924c887ff38e43fcf30ced67155f
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2018-07-31 12:00:29 +0200

    remove test indices after the test finishes

--- src/test/kotlin/io/inbot/search/escrud/AbstractElasticSearchTest.kt
+++ src/test/kotlin/io/inbot/search/escrud/AbstractElasticSearchTest.kt
@@ -2,19 +2,29 @@ package io.inbot.search.escrud
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.http.HttpHost
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest
 import org.elasticsearch.client.RestClient
 import org.elasticsearch.client.RestHighLevelClient
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 
 open class AbstractElasticSearchTest {
     lateinit var dao: IndexDAO<TestModel>
     lateinit var esClient: RestHighLevelClient
+    lateinit var indexName: String
 
     @BeforeEach
     fun before() {
         val restClientBuilder = RestClient.builder(HttpHost("localhost", 9999, "http"))
         esClient = RestHighLevelClient(restClientBuilder)
         // each test gets a fresh index
-        dao = esClient.crudDao("test-" + randomId(), refreshAllowed = true, modelReaderAndWriter = JacksonModelReaderAndWriter(TestModel::class, ObjectMapper().findAndRegisterModules()))
+        indexName = "test-" + randomId()
+        dao = esClient.crudDao(indexName, refreshAllowed = true, modelReaderAndWriter = JacksonModelReaderAndWriter(TestModel::class, ObjectMapper().findAndRegisterModules()))
+    }
+
+    @AfterEach
+    fun after() {
+        // delete the index after the test
+        esClient.indices().delete(DeleteIndexRequest(indexName))
     }
 }

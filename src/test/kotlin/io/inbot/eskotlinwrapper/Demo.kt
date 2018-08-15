f2d04f29d42e711cf5a5bb635128b8c32dc0d17f
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2018-08-15 20:28:25 +0200

    improve demo a bit

--- src/test/kotlin/io/inbot/eskotlinwrapper/Demo.kt
+++ src/test/kotlin/io/inbot/eskotlinwrapper/Demo.kt
@@ -11,18 +11,83 @@ import org.elasticsearch.search.builder.SearchSourceBuilder
 import java.io.File
 import java.nio.charset.StandardCharsets
 
-data class KotlinFile(val name: String, val directory: String, val size: Long, val content: String)
+data class SourceFile(
+    val name: String,
+    val extension: String,
+    val directory: String,
+    val size: Long,
+    val content: String
+)
 
 fun main(args: Array<String>) {
     val restClientBuilder = RestClient.builder(HttpHost("localhost", 9999, "http"))
     val esClient = RestHighLevelClient(restClientBuilder)
+    val dao = esClient.crudDao<SourceFile>("demo")
 
-    esClient.indices().delete(DeleteIndexRequest("demo"))
-    esClient.indices().create(CreateIndexRequest("demo").mapping("demo", """
+    reindex(esClient, dao)
+
+//    listFiles(dao)
+
+//    breakItDown(dao, "directory")
+//    breakItDown(dao, "extension")
+//
+//    findFiles(dao, "bulk")
+}
+
+fun findFiles(dao: IndexDAO<SourceFile>, term: String) {
+    println("All files containing the word bulk")
+    println(dao.search {
+        source(SearchSourceBuilder.searchSource().query(MatchQueryBuilder("content", term)))
+    }.searchResponse.stringify(true))
+}
+
+fun breakItDown(dao: IndexDAO<SourceFile>, field: String) {
+    val results = dao.search {
+        source(
+"""
+{
+    "size": 0,
+    "aggs": {
+        "by_directory": {
+            "terms": {
+                "field":"$field"
+            }
+        }
+    }
+}
+""".trimIndent()
+        )
+    }
+
+    println(
+"""
+Top $field's:
+
+${results.searchResponse.stringify(true)}
+
+""".trimIndent()
+    )
+}
+
+fun reindex(
+    esClient: RestHighLevelClient,
+    dao: IndexDAO<SourceFile>
+) {
+    try {
+        esClient.indices().delete(DeleteIndexRequest("demo"))
+    } catch (e: Exception) {
+        println("so it did not exist ...")
+    }
+    esClient.indices().create(
+        CreateIndexRequest("demo").mapping(
+            "demo", """
 {
     "demo": {
         "properties": {
             "name": {
+                "type":"keyword"
+            },
+            "extension": {
                 "type":"keyword"
             },
             "directory": {
@@ -32,49 +97,36 @@ fun main(args: Array<String>) {
     }
 }
 
-""".trimIndent(), XContentType.JSON))
+""".trimIndent(), XContentType.JSON
+        )
+    )
 
-    val dao = esClient.crudDao<KotlinFile>("demo")
+    val allowedExtensions = setOf("java", "kt", "py", "sh")
 
     dao.bulk {
-        File("./src").walk().forEach {
-            if (it.isFile) {
-                index(it.name, KotlinFile(it.name, it.parentFile.absolutePath, it.length(), it.readText(StandardCharsets.UTF_8)), create = false)
+        File("/Users/jillesvangurp/git").walk().forEach {
+            if (it.isFile && allowedExtensions.contains(it.extension)) {
+                println("indexing ${it.name}")
+                index(
+                    it.name,
+                    SourceFile(
+                        it.name,
+                        it.extension,
+                        it.parentFile.canonicalPath,
+                        it.length(),
+                        it.readText(StandardCharsets.UTF_8)
+                    ),
+                    create = false
+                )
             }
         }
     }
+    println("done!\n\n")
+}
 
-    println("a list of stuff we indexed\n\n")
-    dao.search(scrollTtlInMinutes = 1) { }.mappedHits.forEach {
+fun listFiles(dao: IndexDAO<SourceFile>) {
+    println("a list of stuff we indexed\n")
+    dao.search(scrolling = true, scrollTtlInMinutes = 1) { }.mappedHits.forEach {
         println("${it.directory} - ${it.name}: ${it.size}")
     }
-
-    val results = dao.search {
-        source(
-            """
-                {
-                    "size": 0,
-                    "aggs": {
-                        "by_directory": {
-                            "terms": {
-                                "field":"directory"
-                            }
-                        }
-                    }
-                }
-            """.trimIndent()
-        )
-    }
-
-    println("""
-        Top directories by file count:
-
-        ${results.searchResponse.stringify(true)}
-
-        """.trimIndent())
-
-    println("All files containing the word bulk")
-    println(dao.search {
-        source(SearchSourceBuilder.searchSource().query(MatchQueryBuilder("content", "bulk")))
-    }.searchResponse.stringify(true))
 }

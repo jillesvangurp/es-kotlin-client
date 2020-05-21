3ef7d5e88b612ce90e1725b63726447ed84db1a5
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2020-05-21 11:48:23 +0200

    rearrange a bit

--- src/test/kotlin/io/inbot/eskotlinwrapper/manual/ManualOverviewPageTest.kt
+++ src/test/kotlin/io/inbot/eskotlinwrapper/manual/ManualOverviewPageTest.kt
@@ -121,19 +121,28 @@ class ManualOverviewPageTest {
                 library written in Kotlin that 
                 adapts the [Highlevel Elasticsearch HTTP client for Java](https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high.html) provided by Elasticsearch.
                 
+            ## Chapters
+            
+            """
+            +pages.joinToString("\n") { "- ${io.inbot.eskotlinwrapper.manual.mdLink(it)}" }
+            +"""
+                
                 ## Introduction
                 
                 The official Java client provides client functionality for essentially everything exposed by their REST
-                API. The Elasticsearch Kotlin Client makes using this functionality more Kotlin friendly. It does this
-                through extension functions that add many useful features and shortcuts. It adds Kotlinn DSLs for
-                querying, defining mappings, and bulk indexing. 
+                API. The Elasticsearch Kotlin Client makes using this functionality more Kotlin friendly. 
+
+                It does this
+                through extension functions that add many useful features and shortcuts. It adds Kotlin DSLs for
+                querying, defining mappings, and bulk indexing. To facilitate the most common use cases, this library
+                also provides a Repository abstraction that enables the user to interact with an index in a way that
+                is less boilerplate heavy.
                 
                 Additionally, it provides co-routine friendly versions of the asynchronous clients in the Java library.
-                This enables the user to write fully reactive code in e.g. Ktor or Spring Boot.
-                
-                ## Manual
+                This enables the user to write fully reactive code in e.g. Ktor or Spring Boot. This makes this
+                library the easiest way to do this currently.
             """
-            +pages.joinToString("\n") { "- ${mdLink(it)}" }
+
         }
     }
 }

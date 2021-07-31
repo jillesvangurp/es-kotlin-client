026e824b14c5908f1f5c642a19989d508f4c70d5
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2021-07-31 10:47:43 +0200

    tweak

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # brew install ktlint & pandoc for this to work
 rm -rf docs epub
-./gradlew build dokkaHtml --rerun-tasks
+./gradlew build dokkaHtml 
 cd epub
 sh create_ebook.sh
 cp book.epub ..
